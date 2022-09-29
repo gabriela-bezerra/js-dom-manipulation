0387458970ff051d56948fdc63927db0a3bbefaf
Gabriela <gabrielaa.bdasilva@gmail.com>
2022-09-28 21:15:36 -0700

    All functions Completed

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 'use strict';
 
-const colorChange = () => {
+
+function changeColor() {
     const colorClass = document.querySelectorAll('.color-change');
 
-    colorClass.classList.add('red')
+    for (const color of colorClass) {
+        color.classList.add('red');
+    }
 }
+
+function validateInput(evt) {
+    evt.preventDefault();
+
+    const numberInput = document.querySelector('#number-input');
+    const userNum = Number(number.value);
+
+    const formFeedback = document.querySelector('#formFeedback')
+
+    if (isNaN(userNum) || userNum >= 10) {
+        formFeedback.innerText = 'Please enter a smaller number.';
+    }
+    else {
+        formFeedback.inneText = 'You are good to go!';
+    }
+}
+
+document.querySelector('.color-changer').addEventListener('click', changeColor);
+document.querySelector('.number-form').addEventListener('submit', validateInput);
